5e54ee657bc80ee488451a53c61f432a2ea44f9e
mlgross <mlgross@Siberia>
2015-11-24 20:01:13 -0200

    persist servico OK

---
+++ src/br/edu/ifsul/testes/TestaInserirServico.java
@@ -0,0 +1,43 @@
+package br.edu.ifsul.testes;
+
+import br.edu.ifsul.modelo.Reparador;
+import br.edu.ifsul.modelo.Carro;
+import br.edu.ifsul.modelo.Servico;
+import java.util.Calendar;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author mlgross 
+ */
+public class TestaInserirServico {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EntityManagerFactory emf
+                = Persistence.createEntityManagerFactory("Revenda-ModelPU");
+        EntityManager em = emf.createEntityManager();
+
+        Reparador reparador = em.find(Reparador.class, 327498234);       
+        Carro carro = em.find(Carro.class, 985798354);
+        
+        Servico servico = new Servico();
+
+        servico.setCarro(carro);
+        servico.setReparador(reparador);        
+        servico.setCusto(250.0);
+        servico.setDescricao("Troca de escapamento");
+        //modify tipe of data
+        servico.setDuracao(Calendar.getInstance());
+        servico.setInsumos("Jogo de Escapamento");
+        
+        em.getTransaction().begin();
+        em.persist(servico);
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
+    }    
+}
